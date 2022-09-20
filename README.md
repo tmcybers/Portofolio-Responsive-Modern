f1e093eb4a86c8bccc039a79e43484fb483b6024
tmcybers <tmcyberagent@gmail.com>
2022-09-20 08:28:35 +0200

    finished

---
+++ README.md
@@ -0,0 +1,19 @@
+* Portofolio creado con HTML, CSS, SASS, JS.
+* Respuesta completa de alto grado con todos los dispositivos moviles, tablets y  dispositivos desktop.
+* Animaciones con CSS.
+* Modo claro y oscuro.
+* Interfaz hermosa y agradable.
+
+Uso:
+
+- Abre ``index.html`` con tu editor favorito. (Te recomiendo usar [VS Code](https://code.visualstudio.com))
+- Busca ``EDITAR`` y reemplaza todo los campos que tengar la palabra EDITAR, tambien busca ``OPCIONAL`` si quieres algunos ajustes más.
+- En la carpeta ``assets/js`` abre el archivo ``main.js`` y busca ``EDITAR``, ahora cambia los campos que te indica.
+- Puedes reeemplazar tu ``CV`` en la carpeta ``assets/pdf``.
+- Reemplaza las imágenes en ``assets/img`` manteniendo el mismo nombre y formato de archivo.
+- Guarda todo los cambios y sube la carpeta a un repositorio de Github y activa Github Pages en las configuraciones.
+- Listo, ahora compartelo con tus colegas.
+
+
+
+
---
+++ assets/js/main.js
@@ -0,0 +1,165 @@
+/*=============== FILTERS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tc =>{
+            tc.classList.remove('filters__active')
+        })
+        target.classList.add('filters__active')
+
+        tabs.forEach(t =>{
+            t.classList.remove('filter-tab-active')
+        })
+        tab.classList.add('filter-tab-active')
+    })
+})
+
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+})
+
+sr.reveal(`.profile__border`)
+sr.reveal(`.profile__name`, {delay: 500})
+sr.reveal(`.profile__profession`, {delay: 600})
+sr.reveal(`.profile__social`, {delay: 700})
+sr.reveal(`.profile__info-group`, {interval: 100, delay: 700})
+sr.reveal(`.profile__buttons`, {delay: 800})
+sr.reveal(`.filters__content`, {delay: 900})
+sr.reveal(`.filters`, {delay: 1000})
+
+// Script para obtener y mostrar todos los repositorios de github
+
+const username = 'tmcybers'; // EDITAR - Cambialo por tu nombre de usuario de Github.
+const maxPages = 1;
+const repoList = document.querySelector('.repos__list');
+const reposSection = document.querySelector('.repos');
+
+// get list of user's public repos
+const getRepos = async function () {
+    let repos = [];
+    let res;
+    for (let i = 1; i <= maxPages; i++) {
+        res = await fetch(
+            `https://api.github.com/users/${username}/repos?&sort=pushed&per_page=100&page=${i}`
+            // {
+            //     headers: {
+            //         Accept: 'application/vnd.github+json',
+            //         Authorization:
+            //             'token your-personal-access-token-here'
+            //     }
+            // }
+        );
+        let data = await res.json();
+        repos = repos.concat(data);
+    }
+    displayRepos(repos);
+};
+getRepos();
+
+// Mostrar la liste de todos los repositorios
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+        let listItem = document.createElement('li');
+        listItem.classList.add('repo');
+        listItem.innerHTML = `
+            <a href="${repo.html_url}"><img src="https://opengraph.githubassets.com/1/${username}/${repo.name}"></a>
+            <span class="repos__text">" ${repo.description} "</span> <br/><br/>
+            <span class="repos__text">${devicons[repo.language]}</span> <br />
+            <br />
+            <a class="repos__button" href="${repo.html_url}" target="_blank">Ver Proyecto <i class="ri-arrow-right-up-line"></i></a> <br />
+            <br />`;
+        repoList.append(listItem);
+    }
+};
+
+// Para los íconos de los lenguajes de programación
+const devicons = {
+    Assembly: '<i class="devicon-labview-plain colored"></i> Assembly',
+    'C#': '<i class="devicon-csharp-plain colored"></i> C#',
+    'C++': '<i class="devicon-cplusplus-plain colored"></i> C++',
+    C: '<i class="devicon-c-plain colored"></i> C',
+    Clojure: '<i class="devicon-clojure-plain colored"></i> C',
+    CoffeeScript: '<i class="devicon-coffeescript-plain colored"></i> CoffeeScript',
+    Crystal: '<i class="devicon-crystal-plain colored"></i> Crystal',
+    CSS: '<i class="devicon-css3-plain colored"></i> CSS',
+    Dart: '<i class="devicon-dart-plain colored"></i> Dart',
+    Dockerfile: '<i class="devicon-docker-plain colored"></i> Docker',
+    Elixir: '<i class="devicon-elixir-plain colored"></i> Elixir',
+    Elm: '<i class="devicon-elm-plain colored"></i> Elm',
+    Erlang: '<i class="devicon-erlang-plain colored"></i> Erlang',
+    'F#': '<i class="devicon-fsharp-plain colored"></i> F#',
+    Go: '<i class="devicon-go-plain colored"></i> Go',
+    Groovy: '<i class="devicon-groovy-plain colored"></i> Groovy',
+    HTML: '<i class="devicon-html5-plain colored"></i> HTML',
+    Haskell: '<i class="devicon-haskell-plain colored"></i> Haskell',
+    Java: '<i class="devicon-java-plain colored" style="color: #ffca2c"></i> Java',
+    JavaScript: '<i class="devicon-javascript-plain colored"></i> JavaScript',
+    Julia: '<i class="devicon-julia-plain colored"></i> Julia',
+    'Jupyter Notebook': '<i class="devicon-jupyter-plain colored"></i> Jupyter',
+    Kotlin: '<i class="devicon-kotlin-plain colored" style="color: #796bdc"></i> Kotlin',
+    Latex: '<i class="devicon-latex-plain colored"></i> Latex',
+    Lua: '<i class="devicon-lua-plain-wordmark colored" style="color: #0000d0"></i> Lua',
+    Matlab: '<i class="devicon-matlab-plain colored"></i> Matlab',
+    Nim: '<i class="devicon-nixos-plain colored" style="color: #FFC200"></i> Nim',
+    Nix: '<i class="devicon-nixos-plain colored"></i> Nix',
+    ObjectiveC: '<i class="devicon-objectivec-plain colored"></i> ObjectiveC',
+    OCaml: '<i class="devicon-ocaml-plain colored"></i> OCaml',
+    Perl: '<i class="devicon-perl-plain colored"></i> Perl',
+    PHP: '<i class="devicon-php-plain colored"></i> PHP',
+    PLSQL: '<i class="devicon-sqlite-plain colored"></i> PLSQL',
+    Processing: '<i class="devicon-processing-plain colored" style="color: #0096D8"></i> Processing',
+    Python: '<i class="devicon-python-plain colored" style="color: #3472a6"></i> Python',
+    R: '<i class="devicon-r-plain colored"></i> R',
+    Ruby: '<i class="devicon-ruby-plain colored"></i> Ruby',
+    Rust: '<i class="devicon-rust-plain colored" style="color: #DEA584"></i> Rust',
+    Sass: '<i class="devicon-sass-original colored"></i> Sass',
+    Scala: '<i class="devicon-scala-plain colored"></i> Scala',
+    Shell: '<i class="devicon-bash-plain colored" style="color: #89E051"></i> Shell',
+    Solidity: '<i class="devicon-solidity-plain colored"></i> Solidity',
+    Stylus: '<i class="devicon-stylus-plain colored"></i> Stylus',
+    Svelte: '<i class="devicon-svelte-plain colored"></i> Svelte',
+    Swift: '<i class="devicon-swift-plain colored"></i> Swift',
+    Terraform: '<i class="devicon-terraform-plain colored"></i> Terraform',
+    TypeScript: '<i class="devicon-typescript-plain colored"></i> TypeScript',
+    'Vim Script': '<i class="devicon-vim-plain colored"></i> Vim Script',
+    Vue: '<i class="devicon-vuejs-plain colored"></i> Vue',
+    null: '<i class="devicon-markdown-original"></i> Markdown'
+};
